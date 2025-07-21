5196e447909cfab1c1935940d43d203f6c38ea95
fadilm777 <fadil.m777@gmail.com>
2025-07-21 14:30:16 -0400

    new socket format

--- pyInterface/src/socketConn.py
+++ pyInterface/src/socketConn.py
@@ -20,14 +20,24 @@ class UDPsender:
         assert self.sock is None
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    def _prep_message(self, data) -> str:
+        message = ""
+
+        for key, value in data.items():
+            msg_str = f'{key}:{value},'
+            message += msg_str
+
+        return message[:-1]
+
     def start_service(self) -> None:
         controllers = self.ingescapeDelegate.controllers
         old_message = {}
 
         while True:
-            message = "hello"
-            encoded_message = message.encode()
+            if controllers != old_message:
+                message = self._prep_message(copy.deepcopy(controllers))
+                encoded_message = message.encode()
 
-            assert self.sock is not None
-            self.sock.sendto(encoded_message, (self.IP, self.port))
-            # old_message = copy.deepcopy(message)
+                assert self.sock is not None
+                self.sock.sendto(encoded_message, (self.IP, self.port))
+                old_message = copy.deepcopy(controllers)
