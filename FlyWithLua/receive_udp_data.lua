795383903e8b080697cdbc5517a6da401631c781
fadilm777 <fadil.m777@gmail.com>
2025-07-22 14:35:06 -0400

    fixed lua script

--- FlyWithLua/receive_udp_data.lua
+++ FlyWithLua/receive_udp_data.lua
@@ -4,19 +4,19 @@ local socket = require("socket")
 -- Set position datarefs (writable)
 dataref("override_gps", "sim/operation/override/override_gps", "writable")
 dataref("COURSE", "laminar/B738/autopilot/course_pilot", "writable")
+dataref("COURSE_CO", "laminar/B738/autopilot/course_copilot", "writable")
+
 
 local controllers = {
 	course = function(val)
 		COURSE = val
+		COURSE_CO = val
 	end,
 }
 
 local udp = assert(socket.udp())
 udp:setsockname("127.0.0.1", 7070)
-
-print("UDP listener on port 7070...")
-
-udp:settimeout(nil)
+udp:settimeout(0)
 
 -- Regex separator
 function split(str, sep)
@@ -29,19 +29,22 @@ end
 
 function msgMiddleware(data)
 	local controllerItems = split(data, ",")
-	for _, controllerItem in ipairs(controllerItems) do
-		local name, value = table.unpack(split(controllerItem, ":"))
 
+	for _, controllerItem in ipairs(controllerItems) do
+		local name, value = unpack(split(controllerItem, ":"))
+		
 		if controllers[name] ~= nil then
 			controllers[name](value)
 		end
 	end
 end
 
-while true do
+function receive_udp_data()
 	local data, _, _ = udp:receivefrom()
 
 	if data then
 		msgMiddleware(data)
 	end
 end
+
+do_every_frame("receive_udp_data()")
